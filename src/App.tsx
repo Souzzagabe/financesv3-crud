0f74572b5a484f6caf4b74147135941861e0d490
Gabriel <souzzagabe@hotmail.com>
2024-03-10 17:16:07 -0300

    feat: criação do arquivo helpers com as funções de filtro por data

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { useState, useEffect } from "react";
 import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { Category } from "./types/Category";
+import { categories } from "./data/catogories";
+import { items } from "./data/items";
+import { getCurrentMonth } from "./helpers/dateFilter";
 
 function App() {
+
+  const [list, setList] = useState(items)
+  const [filteredList, setFilteredList] = useState<Item[]>([])
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth())
+
+  useEffect(()=> {
+    
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-        
+
       </C.Body>
     </C.Container>
   );
